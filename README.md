ac714324e815901361f0cf7782065ae13ad7f1eb
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-28 17:40:16 -0500

    shes alive

---
+++ README.md
@@ -0,0 +1,14 @@
+---Monsterhearts Classroom Tool!
+
+This tool allows you to represent a classroom of 15 students. You can sit them anywhere, name them, and color them (the button cycles through some preset colors, more may be added by adding classes to the styles.css and referencing them in the colors array in the script.js file)
+
+Each player should choose a color to represent them and where they sit, proximity can influence how well a character knows an npc!
+
+There's currently no plans to add bespoke threads to the tool, as we would like to keep things as neat and tidy in the ui as possible
+
+--planned updates:
+
+a button to add input fields in the navbar, possibly a way of highlighting the chair your in as a bit of razzle dazzle
+
+--known bugs:
+no currently known bugs, but testing has been minimal, so if there are any feel free to let me know!
--- script.js
+++ script.js
@@ -6,17 +6,29 @@ const colors = [
     'orange',
     'dead'
    ] 
-let colorIndex = 0;
 
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => {
         const box = button.closest('.box')
+        const classes= box.classList;
+        const colorClass = classes[classes.length - 1];
+        let colorIndex = 0;
+
+        //initializes colorIndex from 0
+        if (colorClass) {
+            console.log('yusss')
+            colorIndex = colors.indexOf(colorClass);
+            console.log(colorIndex)
+        };
+    
+        //case in which we're already at the end of the array
         if (colorIndex >= colors.length -1) {
             colorIndex = 0;
             box.classList.remove(colors[colors.length-1])
             box.classList.add(colors[colorIndex])
             return;
-        }
+        };
+        //remove current, increment index, add next 
         if (box.classList.contains(colors[colorIndex])) {
             box.classList.remove(colors[colorIndex])
             colorIndex++;
